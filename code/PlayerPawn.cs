341b30d4b418a9f04b19f585e4af008dad8c8f9e
TaylorrFP <taylorreynolds@facepunchstudios.com>
2024-02-07 19:25:15 +0000

    player colours get shuffled now

--- code/PlayerPawn.cs
+++ code/PlayerPawn.cs
@@ -1,10 +1,12 @@
 ﻿using Sandbox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class PlayerPawn : Component
 {
-	public Color[] playerColours = new Color[19];
+	[Sync] public NetList<Color> playerColours { get; set; } = new();
+
 	//string[] playerEmojis = new string[19];
 
 	[Property] public GameObject playerCursorPrefab { get; set; }
@@ -49,6 +51,20 @@ public class PlayerPawn : Component
 
 	Vector3 WishVelocity = Vector3.Zero;
 
+
+
+
+	void Shuffle<T>( NetList<T> inputList )
+	{
+		for ( int i = 0; i < inputList.Count - 1; i++ )
+		{
+			T temp = inputList[i];
+			int rand = Random.Shared.Int( i, inputList.Count-1 ); //Random.Range( i, inputList.Count );
+			inputList[i] = inputList[rand];
+			inputList[rand] = temp;
+		}
+	}
+
 	protected override void OnAwake()
 	{
 	
@@ -58,25 +74,38 @@ public class PlayerPawn : Component
 
 
 		//do this better in the future
-		playerColours[0] = new Color( 0.956f, 0.262f, 0.211f );
-		playerColours[1] = new Color( 0.913f, 0.117f, 0.388f);
-		playerColours[2] = new Color( 0.611f, 0.152f, 0.690f );
-		playerColours[3] = new Color( 0.403f, 0.227f, 0.717f );
-		playerColours[4] = new Color( 0.247f, 0.317f, 0.709f );
-		playerColours[5] = new Color( 0.129f, 0.588f, 0.952f );
-		playerColours[6] = new Color( 0.011f, 0.662f, 0.956f );
-		playerColours[7] = new Color( 0f, 0.737f, 0.831f );
-		playerColours[8] = new Color( 0f, 0.588f, 0.533f );
-		playerColours[9] = new Color( 0.298f, 0.298f, 0.313f );
-		playerColours[10] = new Color( 0.545f, 0.764f, 0.290f );
-		playerColours[11] = new Color( 0.803f, 0.862f, 0.223f );
-		playerColours[12] = new Color( 1f, 0.921f, 0.231f );
-		playerColours[13] = new Color( 1f, 0.756f, 0.02f );
-		playerColours[14] = new Color( 1f, 0.596f, 0f );
-		playerColours[15] = new Color( 1f, 0.341f, 0.133f );
-		playerColours[16] = new Color( 0.474f, 0.333f, 0.282f );
-		playerColours[17] = new Color( 0.619f, 0.619f, 0.619f );
-		playerColours[18] = new Color( 0.376f, 0.490f, 0.545f );
+		playerColours.Add( new Color( 0.956f, 0.262f, 0.211f ));
+		playerColours.Add( new Color( 0.913f, 0.117f, 0.388f) );
+		playerColours.Add( new Color( 0.611f, 0.152f, 0.690f ) );
+		playerColours.Add( new Color( 0.403f, 0.227f, 0.717f ) );
+		playerColours.Add( new Color( 0.247f, 0.317f, 0.709f ) );
+		playerColours.Add( new Color( 0.129f, 0.588f, 0.952f ) );
+		playerColours.Add( new Color( 0.011f, 0.662f, 0.956f ) );
+		playerColours.Add( new Color( 0f, 0.737f, 0.831f ) );
+		playerColours.Add( new Color( 0f, 0.588f, 0.533f ) );
+		playerColours.Add( new Color( 0.298f, 0.298f, 0.313f ) );
+		playerColours.Add( new Color( 0.545f, 0.764f, 0.290f ) );
+		playerColours.Add( new Color( 0.803f, 0.862f, 0.223f ) );
+		playerColours.Add( new Color( 1f, 0.921f, 0.231f ) );
+		playerColours.Add( new Color( 1f, 0.756f, 0.02f ) );
+		playerColours.Add( new Color( 1f, 0.596f, 0f ) );
+		playerColours.Add( new Color( 1f, 0.341f, 0.133f ) );
+		playerColours.Add( new Color( 0.474f, 0.333f, 0.282f ) );
+		playerColours.Add( new Color( 0.619f, 0.619f, 0.619f ) );
+		playerColours.Add( new Color( 0.376f, 0.490f, 0.545f ) );
+
+
+		if ( !IsProxy )
+		{
+
+			//shuffle the list of colours
+			//but make sure this is networked
+
+			Shuffle( playerColours );
+			
+
+		}
+
 
 		//playerEmojis[0] = "🍎";
 		//playerEmojis[1] = "🐷";
